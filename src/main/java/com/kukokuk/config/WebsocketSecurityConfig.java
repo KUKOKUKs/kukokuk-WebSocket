3280467e75377fe7676d1f955fca04bc003d2ccf
jun-geun-choi <jklucky73@gmail.com>
2025-11-03 21:58:51 +0900

    251103-mod- 시큐리티 이름 변경

--- src/main/java/com/kukokuk/config/WebsocketSecurityConfig.java
+++ src/main/java/com/kukokuk/config/WebsocketSecurityConfig.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class WebsocketSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http,
